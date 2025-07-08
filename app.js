9289ddfa99ca26a7d353056ac13c8baec49b629f
Bigben4 <130978886+Bigben4@users.noreply.github.com>
2025-07-08 01:04:28 -0700

    confuring api

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+const cityInput = document.querySelector('.city');
+const weatherresult=queryselector('#weather-result');
+const loadingIcon=queryselector('#loading-icon');
+const errorIcon=queryselector('#error-icon');
+const apiKey
